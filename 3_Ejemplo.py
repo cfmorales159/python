998ea3fb6e35d58e8cc075f86fdc4b2eb25a002e
claudio federico morales <matute@MacBook-Air-de-claudio.local>
2025-09-06 07:29:34 -0300

    ejercicios_libro listas

---
+++ 3_Ejemplo.py
@@ -0,0 +1,4 @@
+bycicles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bycicles[0])  # Imprime el primer elemento de la lista
+print(bycicles[0].title())  # Imprime el primer elemento con la primera letra en mayúscula
+print(bycicles[1])  # Imprime el segundo elemento
